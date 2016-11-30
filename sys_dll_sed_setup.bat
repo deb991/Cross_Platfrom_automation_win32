27271536bd58715831a6e201de41e2306b22448a
Ozzius <deb991@users.noreply.github.com>
2016-11-30 05:58:25 +0530

    sys_dll_sed_setup.bat
    This is the file to set up sed command in windows environment.

---
+++ sys_dll_sed_setup.bat
@@ -0,0 +1,11 @@
+xcopy C:\Users\%username%\Documents\AUTOMATION\packages\msvcrt.dll C:\Windows\system\
+xcopy C:\Users\%username%\Documents\AUTOMATION\packages\msvcp60.dll C:\Windows\system\
+xcopy C:\Users\%username%\Documents\AUTOMATION\packages\libiconv2.dll C:\Windows\system\
+xcopy C:\Users\%username%\Documents\AUTOMATION\packages\libintl3.dll C:\Windows\system\
+xcopy C:\Users\%username%\Documents\AUTOMATION\packages\regex2.dll C:\Windows\system\
+
+##cd C:\Windows\system\ && del msvcrt.dll
+##cd C:\Windows\system\ && del msvcp60.dll
+##cd C:\Windows\system\ && del libiconv2.dll
+##cd C:\Windows\system\ && del libintl3.dll
+##cd C:\Windows\system\ && del regex2.dll
